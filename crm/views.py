5f4d1880c99785c22c3dba1c8dcbb5df473ae241
yamako-tech <t_yamamoto@ask946.com>
2022-08-27 23:00:22 +0900

    Set ddate

--- crm/views.py
+++ crm/views.py
@@ -6,11 +6,17 @@ from .models import HomeMessage
 
 def index(request):
   context = {}
-  my_date = request.POST.get('my_date','2022-08-24') # for eg. 2022-0826
-  my_date = parse(my_date)
-  # my_date = datetime.strptime(my_date, '%Y-%m-%d')
+  my_date = request.POST.get('my_date','') # for eg. 2022-0826
+  my_date = str(my_date)
+  # my_date = parse(my_date)
+  my_date = datetime.strptime(my_date, '%Y-%m-%d')
   msg = HomeMessage.objects.filter(start_date__gt=my_date, end_date__lt=my_date)
   context['msg_list'] = msg
+
+
+  # msg = HomeMessage.objects.all().values()
+  # context['msg_list'] = msg
+
 
   return render(request, 'index.html', context)
 
