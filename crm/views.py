e605384ffeb407dee5fa994909d9d9f5ffe58d21
yamako-tech <t_yamamoto@ask946.com>
2022-08-29 20:53:15 +0900

    Add is_published function

--- crm/views.py
+++ crm/views.py
@@ -1,57 +1,16 @@
 from django.shortcuts import render
 import datetime
-from dateutil.parser import parse
-from django.utils import timezone
-
 from .models import HomeMessage
 
 def index(request):
-
-  # my_date = parse(my_date)
-  # # my_date = datetime.strptime(my_date, '%Y-%m-%d')
-  # msg = HomeMessage.objects.filter(start_date__gt=my_date, end_date__lt=my_date)
-  # context['msg_list'] = msg
-
   context = {}
-<<<<<<< HEAD
-  # 1. get viewing date
-  datas = HomeMessage.objects.all().values()
-  # 本日の日付を取得
+  # Get today's date
   my_date= datetime.date.today()
-  # 投稿内容を全て取得
-  msg = HomeMessage.objects.all().values()
-  # 投稿期間中のものだけを表示
-  msg2 = HomeMessage.objects.filter(start_date__lt=my_date, end_date__gt=my_date)
+  
+  # Filter by is_publishe, start_date and end_date
+  msg = HomeMessage.objects.filter(is_published=True, start_date__lt=my_date, end_date__gt=my_date)
 
-=======
-  my_date = request.POST.get('my_date','') # for eg. 2022-0826
-  my_date = str(my_date)
-  # my_date = parse(my_date)
-  my_date = datetime.strptime(my_date, '%Y-%m-%d')
-  msg = HomeMessage.objects.filter(start_date__gt=my_date, end_date__lt=my_date)
->>>>>>> 5f4d1880c99785c22c3dba1c8dcbb5df473ae241
   context['msg_list'] = msg
-  context['today'] = my_date
-  context['dates'] = datas
-  context['msg2'] = msg2
 
-
-  # msg = HomeMessage.objects.all().values()
-  # context['msg_list'] = msg
   return render(request, 'index.html', context)
 
-# 2. get start and end
-# 3. Check if viewing date is between #2
-# 4. If yes, the post is_published -- Show the post
-# 5. If no, the post NOT is_publihed -- Hide the post
-
-    # start_d = HomeMessage.start_date()
-    # end_d = HomeMessage.end_date()
-    # view_d = datetime.now()
-    # date_from = datetime.datetime.strptime(request.GET['start_d'], '%Y-%m-%d %H:%M:%S')
-    # start_date = datetime.strptime(request.GET['HomeMessage.start_date'], '%Y-%m-%d %H:%M:%S')
-    # end_date = datetime.strptime(HomeMessage.end_date, '%Y-%m-%d %H:%M:%S')
-    # view_date = datetime.now()
-    # if end_date < view_date < start_date:
-    #   msg = HomeMessage.objects.all().values()
-    #   context['msg_list'] = msg
